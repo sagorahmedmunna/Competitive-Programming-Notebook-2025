ff8475c0629cc1a1f11fa7da95a4d12f36051c96
Sagor Ahmed Munna <sagorahmedmunna@gmail.com>
2025-10-20 22:57:27 +0600

    update

--- codes/MANACHER.cpp
+++ codes/MANACHER.cpp
@@ -1,12 +1,7 @@
 struct Manacher { // 0 based
-  int n;
-  vector<int> p;
+  int n; vector<int> p;
   Manacher(string& s) {
-    n = s.size();
-    p.resize(2 * n);
-    build(s);
-  }
-  void build(string& s) {
+    n = s.size(); p.resize(2 * n);
     for (int i = 0, j = 0, k; i < n * 2; i += k, j = max(j - k, 0)) {
       while (i >= j && i + j + 1 < n * 2 && s[(i - j) / 2] == s[(i + j + 1) / 2]) ++j;
       p[i] = j;
@@ -21,6 +16,6 @@ struct Manacher { // 0 based
     else idx = (l + len / 2 - 1) * 2 + 1;
     return p[idx] >= len;
   }
-  int odd_length_of_center_i(int i) {return p[i * 2]; }
+  int odd_length_of_center_i(int i) { return p[i * 2]; }
   int even_length_of_center_i(int i) { return p[i * 2 + 1]; }
 };
