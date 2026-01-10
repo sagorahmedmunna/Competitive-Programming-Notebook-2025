e14722d56347f2b3589c41d7b87c0a7c2c62fda7
Sagor Ahmed Munna <sagorahmedmunna@gmail.com>
2026-01-10 12:37:44 +0600

    update

--- codes/SUFFIX_ARRAY.cpp
+++ codes/SUFFIX_ARRAY.cpp
@@ -1,7 +1,7 @@
 /*
   for integer, just change string to vector<int> and minimum value of vector must be >= 1
   for integer, lim will be the maximum value of the array
-  LCP of suffix (sa[i], sa[i + 1]) = lcp[i + 1]
+  LCP of suffix (sa[i], sa[i + 1]) = lcp[i]
   O(nlogn)
 */
 array<vector<int>, 2> get_sa(string& s, int lim = 128) {
@@ -17,13 +17,14 @@ array<vector<int>, 2> get_sa(string& s, int lim = 128) {
     for (int i = 1; i < lim; ++i) ws[i] += ws[i - 1];
     for (int i = n; i--;) sa[--ws[x[y[i]]]] = y[i];
     swap(x, y), p = 1, x[sa[0]] = 0;
-    for (int i = 1; i < n; ++i) a = sa[i - 1], b = sa[i], 
+    for (int i = 1; i < n; ++i) a = sa[i - 1], b = sa[i],
       x[b] = (y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
   }
   for (int i = 1; i < n; ++i) rank[sa[i]] = i;
   for (int i = 0, j; i < n - 1; lcp[rank[i++]] = k)
     for (k && k--, j = sa[rank[i] - 1]; s[i + k] == s[j + k]; k++);
   sa.erase(sa.begin()), lcp.erase(lcp.begin());
+  lcp.push_back(lcp[0]), lcp.erase(lcp.begin());
   return {sa, lcp};
 }
 
